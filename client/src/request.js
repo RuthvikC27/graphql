d4cf51e6ffd354d0caee79e83a89b26551b77bb0
Ruthvik Vanam <ruthvikvanam27@gmail.com>
2020-08-30 00:40:46 +0530

    initial

--- client/src/request.js
+++ client/src/request.js
@@ -1,26 +1,72 @@
 const endpoint_url = 'http://localhost:9000/graphql'
 
-export async function loadJobs() {
+async function graphqlRequest(query, variables) {
   const response = await fetch(endpoint_url, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
     },
     body: JSON.stringify({
-      query: `{
-                jobs{
-                    id,
-                    title,
-                    companies{
-                        id,
-                        title
-                    },
-                },
-            }`,
+      query,
+      variables,
     }),
   })
-
   const responseBody = await response.json()
-  console.log(responseBody)
-  return responseBody.data.jobs
+  if (responseBody.errors) {
+    const message = responseBody.errors.map((error) => error.message).join('\n')
+    throw new Error(message)
+  }
+  //   console.log(responseBody)
+  return responseBody.data
+}
+
+export async function loadCompany(id) {
+  const query = `query companyQuery($id: ID){
+        company(id: $id){
+          id
+          name
+          description
+      }
+      }`
+  const variables = {
+    id,
+  }
+  const response = await graphqlRequest(query, variables)
+  return response.company
+}
+
+export async function loadJob(id) {
+  const query = `
+    query jobQuery($id: ID!){
+        job(id: $id){
+          id
+          title
+          companies{
+            id
+            name
+          }
+          description
+        }
+      }`
+  const variables = {
+    id,
+  }
+  const response = await graphqlRequest(query, variables)
+  return response.job
+}
+
+export async function loadJobs() {
+  const query = `{
+        jobs{
+            id,
+            title,
+            companies{
+                id,
+                name
+            },
+        },
+    }`
+  const variables = {}
+  const response = await graphqlRequest(query, variables)
+  return response.jobs
 }
